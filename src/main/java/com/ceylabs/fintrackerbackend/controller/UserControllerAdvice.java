9b68309bd1dd14487fb36621dded0ec36a0d3b73
th4ruka <tharukapavith2@gmail.com>
2024-10-01 23:34:30 +0530

    Fix: Add custom UserAlreadyExistsException.

---
+++ src/main/java/com/ceylabs/fintrackerbackend/controller/UserControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.ceylabs.fintrackerbackend.controller;
+
+import com.ceylabs.fintrackerbackend.exception.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserControllerAdvice {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+}
+
